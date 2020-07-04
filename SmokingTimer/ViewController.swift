8215300ab9d952fd2911233d7cd921823ba99112
jinn <booboo080yamaya@gmail.com>
2020-07-04 20:19:19 +0900

    Initial Commit

---
+++ SmokingTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmokingTimer
+//
+//  Created by 神野成紀 on 2020/07/04.
+//  Copyright © 2020 神野成紀. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
