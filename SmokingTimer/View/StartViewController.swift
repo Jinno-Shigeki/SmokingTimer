2b6ce8dd1093f6c8794d59572fcef4aadcce4ab5
jinn <booboo080yamaya@gmail.com>
2020-07-15 03:21:48 +0900

    起動時の初期画面の作成

---
+++ SmokingTimer/View/StartViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  SmokingTimer
+//
+//  Created by 神野成紀 on 2020/07/14.
+//  Copyright © 2020 神野成紀. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+    @IBOutlet weak var boxPrice: UITextField!
+    @IBOutlet weak var numberOfDay: UITextField!
+    @IBOutlet weak var okButton: UIButton!
+    
+    let presenter = StartViewPresenter()
+    var UserId = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UserId = presenter.createID()
+        boxPrice.delegate = self
+        numberOfDay.delegate = self
+        self.boxPrice.keyboardType = .numberPad
+        self.numberOfDay.keyboardType = .numberPad
+        createReturnKey()
+        okButton.isEnabled = false
+        okButton.backgroundColor = .lightGray
+    }
+    
+    @IBAction func tapOkButton(_ sender: UIButton) {
+        if boxPrice.text != "" && numberOfDay.text != "" {
+            UserDefaults.standard.set(UserId, forKey: "user")
+            presenter.sendStartData(user: UserId, boxPrice: boxPrice.text!, numberOfDay: numberOfDay.text!)
+            performSegue(withIdentifier: "next", sender: sender)
+        }
+    }
+}
+//MARK: - UITextFieldDelegate
+extension StartViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if boxPrice.text != "" && numberOfDay.text != "" {
+            okButton.isEnabled = true
+            okButton.backgroundColor = UIColor(named: "customGreen")
+        }
+    }
+    
+    func createReturnKey() {
+        let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 50))
+        let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let returnKey = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(tapReturnKey))
+        returnKey.tintColor = .black
+        toolBar.items = [space, returnKey]
+        boxPrice.inputAccessoryView = toolBar
+        numberOfDay.inputAccessoryView = toolBar
+    }
+    @objc func tapReturnKey() {
+        boxPrice.endEditing(true)
+        numberOfDay.endEditing(true)
+    }
+}
+
