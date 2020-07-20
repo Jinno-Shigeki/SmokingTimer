ae5c281b8eabed804e73bf02ff3a619baef2d86d
jinn <booboo080yamaya@gmail.com>
2020-07-20 20:34:39 +0900

    プログレスバー作成

--- SmokingTimer/Presenter/HomeViewPresenter.swift
+++ SmokingTimer/Presenter/HomeViewPresenter.swift
@@ -21,6 +21,9 @@ class HomeViewPresenter {
     private var numberOfBox = UserDefaults.standard.integer(forKey: "numberOfBox")
     private var finishDay = ""
     private var totalSecond = 0
+    private var levelsProgress: Double = 0
+    private let levelCount = [1200, 27600, 57600, 87600, 345600]
+    
     init(view: HomeViewProtocol) {
         self.view = view
     }
@@ -62,18 +65,17 @@ class HomeViewPresenter {
         let startDate = UserDefaults.standard.object(forKey: "Date") as! Date
         let now = Date()
         let dateComponents = Calendar.current.dateComponents([.day, .hour, .minute, .second], from: startDate, to: now)
-        savedMoney = calculateSavedMoney(dateComponents: dateComponents)
+        let totalSecondComp = Calendar.current.dateComponents([.second], from: startDate, to: now)
+        totalSecond = totalSecondComp.second!
+        savedMoney = calculateSavedMoney()
         savedNumber = calculateSavedNumber()
-        view!.upDateTimer(timer: "\(dateComponents.day!)\(StaticData.date) \( dateComponents.hour!)\(StaticData.hour) \( dateComponents.minute!)\(StaticData.minute) \(dateComponents.second!)\(StaticData.second)", money: "\(savedMoney)\(StaticData.money)", number: "\(savedNumber)\(StaticData.number)")
+        view?.upDateTimer(timer: "\(dateComponents.day!)\(StaticData.date) \( dateComponents.hour!)\(StaticData.hour) \( dateComponents.minute!)\(StaticData.minute) \(dateComponents.second!)\(StaticData.second)", money: "\(savedMoney)\(StaticData.money)", number: "\(savedNumber)\(StaticData.number)")
+        view?.upDateLevels(level: getCurrentLevel(dateComponents: dateComponents), progressValue: CGFloat(calculateLevelProgress()))
     }
     
-    func calculateSavedMoney(dateComponents: DateComponents) -> String {
+    func calculateSavedMoney() -> String {
         let unitPrice = boxPrice / numberOfBox
         let secondPerMoney = Double(unitPrice * numberOfDay) / 86400
-        let secondOfDays = dateComponents.day! * 86400
-        let secondOfHours = dateComponents.hour! * 360
-        let secondOfMinutes = dateComponents.minute! * 60
-        totalSecond = secondOfDays + secondOfHours + secondOfMinutes + dateComponents.second!
         let currentMooney = Double(totalSecond) * secondPerMoney
         savedMoney = String(format: "%.2f", currentMooney)
         return savedMoney
@@ -91,7 +93,59 @@ class HomeViewPresenter {
             UserDefaults.standard.removeObject(forKey: "Date")
             UserDefaults.standard.removeObject(forKey: "startTime")
         }
-            UserDefaults.standard.set(date, forKey: "Date")
-            UserDefaults.standard.set(startTime, forKey: "startTime")
+        UserDefaults.standard.set(date, forKey: "Date")
+        UserDefaults.standard.set(startTime, forKey: "startTime")
+    }
+    func getCurrentLevel(dateComponents: DateComponents) -> String {
+        if totalSecond < 1200 {
+            return StaticData.levels[0]
+        } else if totalSecond >= 1200 {
+            return StaticData.levels[1]
+        } else if totalSecond >= 28800 {
+            return StaticData.levels[2]
+        } else if totalSecond >= 86400 {
+            return StaticData.levels[3]
+        } else if totalSecond >= 172800 {
+            return StaticData.levels[4]
+        } else if totalSecond >= 259200 {
+           return StaticData.levels[5]
+        } else if totalSecond >= 604800 {
+            return StaticData.levels[6]
+        } else if totalSecond >= 2592000 {
+            return StaticData.levels[7]
+        }
+        return ""
+    }
+    
+    func calculateLevelProgress() -> Double {
+        var parcentage: Double = 0
+        if totalSecond < 1200 {
+            parcentage = Double(totalSecond ) / Double(levelCount[0])
+            levelsProgress = parcentage * 100
+            return floor(levelsProgress * 100) / 100
+        } else if totalSecond >= 1200 {
+            parcentage = Double(totalSecond - levelCount[1]) / Double(levelCount[1])
+            levelsProgress = parcentage * 100
+            return floor(levelsProgress * 100) / 100
+        } else if totalSecond >= 28800 {
+            parcentage = Double(totalSecond - levelCount[2]) / Double(levelCount[2])
+            levelsProgress = parcentage * 100
+            return floor(levelsProgress * 100) / 100
+        } else if totalSecond >= 86400 {
+            parcentage = Double(totalSecond - levelCount[3]) / Double(levelCount[3])
+            levelsProgress = parcentage * 100
+            return floor(levelsProgress * 100) / 100
+        } else if totalSecond >= 172800 {
+            parcentage = Double(totalSecond - levelCount[4]) / Double(levelCount[4])
+            levelsProgress = parcentage * 100
+            return floor(levelsProgress * 100) / 100
+        } else if totalSecond >= 259200 {
+            parcentage = Double(totalSecond - levelCount[5]) / Double(levelCount[5])
+            levelsProgress = parcentage * 100
+            return floor(levelsProgress * 100) / 100
+        } else if totalSecond >= 604800 {
+            return 100
+        }
+            return 0
     }
 }
